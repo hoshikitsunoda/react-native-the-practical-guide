2e7ff609906db5c9901f3bc717308784c76ec155
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-09 23:30:27 -0700

    project set up

---
+++ my-new-app/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: '80%',
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
