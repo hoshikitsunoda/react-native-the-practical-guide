1a621773b8ca010aea97547fde7268f2bb893a3d
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-09 23:52:33 -0700

    added StyleSheet and styling

--- my-new-app/App.js
+++ my-new-app/App.js
@@ -4,23 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            width: '80%',
-            borderBottomColor: 'black',
-            borderBottomWidth: 1,
-            padding: 10,
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View></View>
@@ -29,4 +15,19 @@ export default function App() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+})
